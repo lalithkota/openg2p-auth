0746356f5bbee02a3348552a2303f38491693cbe
Lalith Kota <kotalalith@gmail.com>
2024-07-26 18:08:55 +0530

    Precommit errors fixed
    Signed-off-by: Lalith Kota <kotalalith@gmail.com>

--- g2p_auth_id_oidc/static/src/js/authentication_status.js
+++ g2p_auth_id_oidc/static/src/js/authentication_status.js
@@ -1,22 +1,22 @@
 /** @odoo-module **/
 
+import AbstractField from "web.AbstractField";
 import {_t} from "@web/core/l10n/translation";
 import fieldsRegistry from "web.field_registry";
-import AbstractField from "web.AbstractField";
 
 var RegIdAuthStatus = AbstractField.extend({
-    template: 'g2p_auth_id_oidc.reg_id_auth_status',
+    template: "g2p_auth_id_oidc.reg_id_auth_status",
     events: {
         "click button": "authenticateButtonClick",
     },
 
-    init: function() {
+    init: function () {
         this._super.apply(this, arguments);
 
         this.statusClass = this.nodeOptions.statusClass;
         this.buttonClass = this.nodeOptions.buttonClass;
 
-        this.showWidget = this.recordData.auth_oauth_provider_id ? true : false;
+        this.showWidget = Boolean(this.recordData.auth_oauth_provider_id);
 
         this.statusSelectionObject = Object.fromEntries(this.field.selection);
 
@@ -30,17 +30,17 @@ var RegIdAuthStatus = AbstractField.extend({
         });
     },
 
-    renderStatus: function() {
-        let status = this.recordData.authentication_status;
+    renderStatus: function () {
+        const status = this.recordData.authentication_status;
         return _t(this.statusSelectionObject[status]);
     },
 
-    authenticateButtonClick: function() {
-        let windowFeatures = `popup,height=${(screen.height * 2) / 3},width=${screen.width / 2}`;
+    authenticateButtonClick: function () {
+        const windowFeatures = `popup,height=${(screen.height * 2) / 3},width=${screen.width / 2}`;
         window.open(this.authProvider.auth_link, "", windowFeatures);
     },
 });
 
 fieldsRegistry.add("g2p_auth_id_oidc.reg_id_auth_status", RegIdAuthStatus);
 
-export {RegIdAuthStatus}
+export {RegIdAuthStatus};
